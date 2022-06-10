9660dc1098436225faf055dc087fe51302428fc0
kunertmichal <miichal95@gmail.com>
2022-06-10 15:03:25 +0200

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <h1 className="mt-10 text-3xl">siema!!</h1>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
