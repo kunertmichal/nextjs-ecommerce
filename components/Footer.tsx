b333876ca4f53b8da863ef6bf8f0b125455af1b6
kunertmichal <miichal95@gmail.com>
2022-06-11 12:35:47 +0200

    add logo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
+import { MainLogo } from "./MainLogo";
+
 export const Footer = () => {
   return (
     <footer className="bg-gray-800 p-5 text-white">
-      <div className="container mx-auto">Footer</div>
+      <div className="container mx-auto">
+        <MainLogo />
+      </div>
     </footer>
   );
 };
