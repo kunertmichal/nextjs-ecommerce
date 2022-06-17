7753419335ceaa0baef11f415b02d1daaec7eb83
kunertmichal <miichal95@gmail.com>
2022-06-17 14:57:02 +0200

    Update product title font-weight

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -16,7 +16,7 @@ export const ProductCard = ({ data }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center shadow-md rounded-lg p-8">
       <img src={data.image} alt={data.title} className="max-h-64" />
-      <h2 className="text-xl mt-4">{data.title}</h2>
+      <h2 className="text-xl font-semibold mt-4">{data.title}</h2>
       <p className="text-gray-400">{data.category}</p>
       <div className="flex mt-8">
         <p className="font-bold mr-9">{data.price}$</p>
