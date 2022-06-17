2a6cd9d6c8a7ea052243ddeec8ccdaf94d2041d2
kunertmichal <miichal95@gmail.com>
2022-06-17 14:57:56 +0200

    ProductCard styling adjustments

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -16,9 +16,9 @@ export const ProductCard = ({ data }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center shadow-md rounded-lg p-8">
       <img src={data.image} alt={data.title} className="max-h-64" />
-      <h2 className="text-xl font-semibold mt-4">{data.title}</h2>
+      <h2 className="text-xl font-semibold mt-12">{data.title}</h2>
       <p className="text-gray-400">{data.category}</p>
-      <div className="flex mt-8">
+      <div className="flex mt-4">
         <p className="font-bold mr-9">{data.price}$</p>
         <p className="text-gray-400">
           <span>{data.rating.rate}</span> / <span>5</span> ({data.rating.count}{" "}
